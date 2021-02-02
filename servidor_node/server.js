d403a21249d70c139619b9d4e2052bd6c579d2c0
Andre Ramos <andre-rammos@hotmail.com>
2021-02-02 20:48:56 +0000

    correcao nas rotas dos assets (css, img, html, js) usando app.use(express.static('public'))

--- servidor_node/server.js
+++ servidor_node/server.js
@@ -53,8 +53,20 @@ var storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 //const upload = multer({ storage: memoreStorage });
 //const upload = multer({ dest: 'static/uploads/' });
-
-//app.use(express.static('public'));
+var path = require('path');
+var options = {
+	dotfiles: 'ignore',
+	etag: false,
+	extensions: ['htm', 'html', 'css', 'js'],
+	index: false,
+	maxAge: '1d',
+	redirect: false,
+	setHeaders: function (res, path, stat) {
+	  res.set('x-timestamp', Date.now())
+	}
+  }
+//app.use(express.static(path.join(__dirname + "static/", 'styles')));
+app.use("/static", express.static(path.join(__dirname, 'static'), options));
 
 // ============================ ROTAS =============================================
 app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
@@ -98,30 +110,9 @@ app.delete('/arquivo/:id/deletar', function(req, res){
     });  
  })
 
-// cria uma rota para fornecer o arquivo qrcode.min.js
-app.get('/qrcode.min.js', function(req, res){	
-	res.set('Content-Type', 'text/javascript');
-	res.sendFile(__dirname + '/static/scripts/qrcode.min.js');   
-});
-
-// cria uma rota para fornecer o arquivo imagem
-app.get('/static/imagens/doc.jpg', function(req, res){	
-	res.sendFile(__dirname + '/static/imagens/fundo_papel.jpg');   
-});
-// cria uma rota para fornecer o arquivo imagem
-app.get('/static/imagens/load.gif', function(req, res){	
-	res.sendFile(__dirname + '/static/imagens/load.gif');   
-});
-// cria uma rota para fornecer o arquivo imagem
-app.get('/static/imagens/download-icon.png', function(req, res){	
-	res.sendFile(__dirname + '/static/imagens/download-icon.png');   
-});
-// cria uma rota para fornecer o arquivo imagem
-app.get('/static/imagens/print-icon.png', function(req, res){	
-	res.sendFile(__dirname + '/static/imagens/print-icon.png');   
-});
-// cria uma rota para fornecer o arquivo de imagem
+ // cria uma rota para fornecer o arquivo de imagem
 app.get('/imagem_do_servidor', function(req, res){	
+	res.set('Content-Type', 'image/jpeg');
 	res.sendFile(__dirname + '/static/uploads/default/' + email_do_usuario_logado + '.jpg');   
 });
 // cria uma rota para fornecer o arquivo de imagem
@@ -131,80 +122,11 @@ app.get('/imagem_do_servidor/download', function(req, res){
 
 // cria uma rota para fornecer o arquivo layout.html
 app.get('/', function(req, res){
-	console.log("https://" + req.headers.host + req.url)
+	//console.log("https://" + req.headers.host + req.url)
 	res.set('Content-Type', 'text/html');
 	res.sendFile(__dirname + '/layout.html');  
 });
-app.get('/acentos', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/layout.html'); 
-});
 
-
-// cria uma rota para fornecer o arquivo imagens
-app.get('/static/imagens/favicon.ico', function(req, res){
-	res.sendFile(__dirname + '/static/imagens/favicon.ico');  
-});
-app.get('/static/styles/estilo.css', function(req, res){
-	res.set('Content-Type', 'text/css');
-	res.sendFile(__dirname + '/static/styles/estilo.css');  
-});
-app.get('/static/styles/extras.css', function(req, res){
-	res.set('Content-Type', 'text/css');
-	res.sendFile(__dirname + '/static/styles/extras.css');  
-});
-
-app.get('/static/styles/print.css', function(req, res){
-	res.set('Content-Type', 'text/css');
-	res.sendFile(__dirname + '/static/styles/print.css');  
-});
-app.get('/static/imagens/logo.svg', function(req, res){
-	res.sendFile(__dirname + '/static/imagens/logo.svg');  
-});
-app.get('/static/imagens/load.gif', function(req, res){
-	res.sendFile(__dirname + '/static/imagens/load.gif');  
-});
-
-
-
-// ROTAS PARA AS PÁGINAS DE CONTEÚDO HTML
-app.get('/static/content/home.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/home.html');  
-});
-app.get('/static/content/terms.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/terms.html');  
-});
-app.get('/static/content/about.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/about.html');  
-});
-app.get('/static/content/preview-and-print.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/preview-and-print.html');  
-});
-app.get('/static/content/premium.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/premium.html'); 
-});
-
-app.get('/static/content/privacy.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/privacy.html'); 
-});
-app.get('/static/content/extras.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/extras.html'); 
-});
-app.get('/static/content/manutencao.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/manutencao.html'); 
-});
-app.get('/static/content/doacao.html', function(req, res){
-	res.set('Content-Type', 'text/html');
-	res.sendFile(__dirname + '/static/content/doacao.html'); 
-});
 
 
 //=========== CONEXAO ============================================================
